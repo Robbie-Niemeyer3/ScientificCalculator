66d52187887e6bbad0ab20be9b0f4efd79d62233
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-06-29 15:54:16 -0400

    added scientific function

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -2,43 +2,49 @@ package com.zipcodewilmington.scientificcalculator;
 
 import java.util.Scanner;
 
+import static com.zipcodewilmington.scientificcalculator.ScientificFeatures.tan;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     public static void main(String[] args) {
-int value = 0;
-boolean exitApp = false;
-        Scanner scanner = new Scanner(System.in);
-
-        while(exitApp == false){
-            Console.println("Welcome to my calculator!");
-            CoreFeature application = new CoreFeature();
-            System.out.println(application.getValue1());
+    int value = 0;
+    boolean exitApp = false;
+    Scanner scanner = new Scanner(System.in);
 
 
-            Integer i = Console.getIntegerInput("Enter your first integer");
-            application.setValue1(i);
-            String s = Console.getStringInput("Choose an operator: * / + -");
-            application.setOperator(s);
-            Integer t = Console.getIntegerInput("Enter your second integer");
-            application.setValue2(t);
-            System.out.println("your answer is: " + application.theAnswer());
-            s = Console.getStringInput("Would you like to continue?");
-            if(s.equalsIgnoreCase("no")){
-                exitApp = true;
-            }else{
-                s = Console.getStringInput("Choose an operator: * / + -");
-                application.setOperator(s);
-                application.setValue1(application.getAnswer());
-                i = Console.getIntegerInput("Enter your second integer");
-                application.setValue2(i);
-                System.out.println("Your answer is : " + application.theAnswer());
-            }
 
-        }
-            scanner.close();
 
+//        while(exitApp == false){
+//            Console.println("Welcome to my calculator!");
+//            CoreFeature application = new CoreFeature();
+//            System.out.println(application.getValue1());
+//
+//
+//            Integer i = Console.getIntegerInput("Enter your first integer");
+//            application.setValue1(i);
+//            String s = Console.getStringInput("Choose an operator: * / + -");
+//            application.setOperator(s);
+//            Integer t = Console.getIntegerInput("Enter your second integer");
+//            application.setValue2(t);
+//            System.out.println("your answer is: " + application.theAnswer());
+//            s = Console.getStringInput("Would you like to continue?");
+//            if(s.equalsIgnoreCase("no")){
+//                exitApp = true;
+//            }else{
+//                s = Console.getStringInput("Choose an operator: * / + -");
+//                application.setOperator(s);
+//                application.setValue1(application.getAnswer());
+//                i = Console.getIntegerInput("Enter your second integer");
+//                application.setValue2(i);
+//                System.out.println("Your answer is : " + application.theAnswer());
+//            }
+//
+//
+//        }
+//        scanner.close();
+//
 
 
 //        Double d = Console.getDoubleInput("Enter a double.");
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,43 @@
 package com.zipcodewilmington.scientific_calculator;
+import com.sun.tools.javac.Main;
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+import com.zipcodewilmington.scientificcalculator.ScientificFeatures;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Scanner;
 
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+
+    //Scientific Tests
+
+    double scientificTest = 5;
+    @Test
+    public void displayBinary(){
+        ScientificFeatures display = new ScientificFeatures();
+        assertEquals(101, display.doubleToBinary(scientificTest));
+    }
+
+    @Test
+    public void displayOctal(){
+        ScientificFeatures display = new ScientificFeatures();
+        assertEquals(5, display.doubleToOctal((scientificTest)));
+    }
+
+    @Test
+    public void displayHexa(){
+        ScientificFeatures display = new ScientificFeatures();
+        assertEquals(5, display.doubleToHexaDecimal(scientificTest));
+    }
+
+    @Test // tan() Test
+    public void tanCalculator(){
+        ScientificFeatures calculator = new ScientificFeatures();
+        assertEquals(-3.3805, (ScientificFeatures.tan(scientificTest)));
+    }
+
 }
