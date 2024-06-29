d137cd11949ed1ed9e3dc882353cb810c27b1ffc
Robbie-Niemeyer3 <robbie.niemeyer3@gmail.com>
2024-06-29 11:45:19 -0400

    added Dev branch

--- src/main/java/com/zipcodewilmington/scientificcalculator/CoreFeature.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CoreFeature.java
@@ -1,24 +1,26 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
 public class CoreFeature {
-    int value1 =0;
-    int value2=0;
+    double value1 = 0;
+    double value2 = 0;
     String operator;
-    int answer;
+    double answer;
 
     public void setAnswer(int answer) {
         this.answer = answer;
     }
 
-    public int getValue1() {
+    public double getValue1() {
         return value1;
     }
 
-    public void setValue1(int value1) {
+    public void setValue1(double value1) {
         this.value1 = value1;
     }
 
-    public int getValue2() {
+    public double getValue2() {
         return value2;
     }
 
@@ -38,22 +40,38 @@ public class CoreFeature {
 
     }
 
-    public int theAnswer(){
-        if(operator.equals("*")){
+    public double theAnswer() {
+        if (operator.equals("*")) {
             answer = value1 * value2;
-        }else if(operator.equals("/")){
+        } else if (operator.equals("/")) {
             answer = value1 / value2;
-        }else if(operator.equals("+")){
+        } else if (operator.equals("+")) {
             answer = value1 + value2;
-        }else if(operator.equals("-")){
+        } else if (operator.equals("-")) {
             answer = value1 - value2;
         }
+
         return answer;
 
-    };
+        public class Square {
+            Double square = Math.pow(storeMemory, 2);
+        }
 
-    public int getAnswer() {
-        return answer;
+        public class Sqrt {
+            Double sqrt = Math.sqrt(storeMemory);
+        }
+
+
+
     }
-}
 
+
+    }
+
+//Add, subtract, multiply, and divide the value on the display by a given number
+//Calculate the square (x2) and square root (√x) of the number on the display *
+//Calculate variable exponentiation (xy)
+//Calculate the inverse of the number on the display (1/x) *
+//Invert the sign of the number on the display (switch between positive and negative)
+//Update the display to Err if an error occurs (eg: Division by zero) *
+//Errors must be cleared before any other operation can take place *
