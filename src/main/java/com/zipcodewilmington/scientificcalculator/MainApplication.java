0131801650afc5511e31b041aee8eb7b102861b5
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-06-29 17:57:43 -0400

    added run app class

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -9,45 +9,18 @@ import static com.zipcodewilmington.scientificcalculator.ScientificFeatures.tan;
  */
 public class MainApplication {
     public static void main(String[] args) {
-    int value = 0;
-    boolean exitApp = false;
-    Scanner scanner = new Scanner(System.in);
+double answer =0;
+
+
+
+    RunApp runApp = new RunApp();
+    runApp.runAppSimpleCalc();
+    answer = runApp.getValue();
+        System.out.println("your last value was: " + answer);
 
 
 
 
-        while(exitApp == false){
-            CoreFeature application = new CoreFeature();
-//            System.out.println(application.getValue1());
-
-
-            Integer i = Console.getIntegerInput("Enter your first integer");
-            application.setValue1(i);
-            String s = Console.getStringInput("Choose an operator: * / + -");
-            application.setOperator(s);
-            Integer t = Console.getIntegerInput("Enter your second integer");
-            application.setValue2(t);
-            System.out.println("your answer is: " + application.theAnswer());
-            String cont = Console.getStringInput("Would you like to continue?");
-            if(cont.equalsIgnoreCase("no")){
-                exitApp = true;
-            }else {
-                while(cont.equalsIgnoreCase("yes")){
-                    s = Console.getStringInput("Choose an operator: * / + -");
-                    application.setOperator(s);
-                    application.setValue1(application.getAnswer());
-                    i = Console.getIntegerInput("Enter your second integer");
-                    application.setValue2(i);
-                    System.out.println("Your answer is : " + application.theAnswer());
-                    cont = Console.getStringInput("Would you like to continue?");
-
-                }
-                 break;
-            }
-
-
-        }
-        scanner.close();
 //
 
 
---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/RunApp.java
@@ -0,0 +1,59 @@
+package com.zipcodewilmington.scientificcalculator;
+
+import java.util.Scanner;
+
+public class RunApp {
+    double value =0;
+    public RunApp() {
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void runAppSimpleCalc(){
+
+        boolean exitApp = false;
+        Scanner scanner = new Scanner(System.in);
+
+
+        while(exitApp == false){
+            CoreFeature application = new CoreFeature();
+//            System.out.println(application.getValue1());
+
+
+            Integer i = Console.getIntegerInput("Enter your first integer");
+            application.setValue1(i);
+            String s = Console.getStringInput("Choose an operator: [ * ]  [ / ] [ + ] [ - ]");
+            application.setOperator(s);
+            Integer t = Console.getIntegerInput("Enter your second integer");
+            application.setValue2(t);
+            System.out.println("your answer is: " + application.theAnswer());
+            String cont = Console.getStringInput("Would you like to continue?");
+            if(cont.equalsIgnoreCase("no")){
+                setValue(application.getAnswer());
+                exitApp = true;
+            }else {
+                while(cont.equalsIgnoreCase("yes")){
+                    s = Console.getStringInput("Choose an operator: [ * ]  [ / ] [ + ] [ - ]");
+                    application.setOperator(s);
+                    application.setValue1(application.getAnswer());
+                    i = Console.getIntegerInput("Enter your second integer");
+                    application.setValue2(i);
+                    System.out.println("Your answer is : " + application.theAnswer());
+                    cont = Console.getStringInput("Would you like to continue?");
+                    setValue(application.getAnswer());
+
+                }
+                break;
+            }
+
+
+        }
+        scanner.close();
+    }
+}
